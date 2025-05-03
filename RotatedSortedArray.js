7c517fb7b2271363148fed96101b7e52a7839f65
Sivananda Balaji D <sivanandabalaji6@gmail.com>
2025-05-03 11:28:57 +0530

    add search in rotated sorted array solution

---
+++ RotatedSortedArray.js
@@ -0,0 +1,32 @@
+//33. Search in Rotated Sorted Array
+
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[left] <= nums[mid]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
+
+const nums = [4, 5, 6, 7, 0, 1, 2],
+  target = 0;
+
+const result = search(nums, target);
+
+console.log(result);
